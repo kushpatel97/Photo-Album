cd22853ff1caefb96ad9fbd08cc7bf21b3345ce0
Kush Patel <Mac@nbp-196-155.nbp.ruw.rutgers.edu>
2018-03-26 12:33:11 -0400

    Starts login screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 	
+import controller.LoginController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,9 +13,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
+			AnchorPane root = (AnchorPane) fxmlLoader.load();
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Song Library");
+			LoginController logincontroller = fxmlLoader.getController();
+			logincontroller.start();
+			
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
---
+++ src/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.io.IOException;
+
+public class LoginController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void start() throws IOException{
+		
+	}
+}
