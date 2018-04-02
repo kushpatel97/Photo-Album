98b1c9c16c1b1af92d197a6a089fb50157a12771
Kush Patel <kushp447@gmail.com>
2018-04-02 16:24:40 -0400

    User login. Direction to admin page

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -2,6 +2,8 @@ package controller;
 
 public class AdminController {
 	
-	
+	public void start() {
+		System.out.println("Admin Page");
+	}
 
 }
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,14 +2,45 @@ package controller;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
 public class LoginController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
-	public void start() throws IOException{
+	@FXML public Button mLogIn;
+	
+	@FXML public TextField tfUsername;
+	
+	public final String adminUserName = "admin";
+
+	public void login(ActionEvent event) throws IOException {
+		
+		//Make lowercase idk????. Also check edge cases
+		String username = tfUsername.getText();
+		Parent sceneManager;
+		System.out.print(username);
+		if(username.equals(adminUserName)) {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Admin.fxml"));
+			sceneManager = (Parent) fxmlLoader.load();
+			AdminController adminController = fxmlLoader.getController();
+			Scene adminScene = new Scene(sceneManager);
+			
+			Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			
+			adminController.start();
+			
+			appStage.setScene(adminScene);
+			
+			appStage.show();
+			System.out.print("Clicked Me");
+		}
+		
 		
 	}
 }
