438168f14d051ff947bbee45ed144e89ee129b67
alouie510 <alexander.s.louie@gmail.com>
2018-04-10 02:04:15 -0400

    caption

--- src/controller/SinglePhotoController.java
+++ src/controller/SinglePhotoController.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import application.Main;
 import javafx.collections.FXCollections;
@@ -15,6 +16,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
@@ -52,6 +54,21 @@ public class SinglePhotoController implements LogoutController {
 		}
 	}
 	
+	public void saveCaption() throws IOException {
+		String caption = tfCaption.getText().trim();
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Caption Confirmation");
+		alert.setHeaderText(null);
+		alert.setContentText("Are you sure you want to change the caption to: " + caption);
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			photo.setCaption(caption);
+			photo.save(photo);
+		} else {
+			return;
+		}
+	}
+	
 	public void addTag() {
 		String tagName = tfTagName.getText().trim();
 		String tagValue = tfTagValue.getText().trim();
@@ -69,6 +86,8 @@ public class SinglePhotoController implements LogoutController {
 	}
 	
 	public void update() {
+		tfCaption.setText(photo.getCaption());
+		
 		File file;
 		if (photo != null) {
 			file = photo.getPic();
--- src/model/Photo.java
+++ src/model/Photo.java
@@ -1,6 +1,11 @@
 package model;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 import java.util.Calendar;
@@ -12,6 +17,8 @@ import java.text.SimpleDateFormat;
 
 public class Photo implements Serializable {
 	private static final long serialVersionUID = 1L;
+	public static final String storeDir = "dat";
+	public static final String storeFile = "users.dat";
 	// public Image image;
 	public String photoname;
 	public File pic;
@@ -96,5 +103,18 @@ public class Photo implements Serializable {
 	@Override
 	public String toString() {
 		return getName();
+	}
+	
+	public static void save(Photo pdApp) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(storeDir + File.separator + storeFile));
+		oos.writeObject(pdApp);
+		oos.close();
+	}
+
+	public static User load() throws IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(storeDir + File.separator + storeFile));
+		User userList = (User) ois.readObject();
+		ois.close();
+		return userList;
 	}
 }
